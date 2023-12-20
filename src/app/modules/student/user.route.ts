208539232bee0fea9e9fd268e42dd716ac324709
Sanny <jshahadat8@gmail.com>
2023-12-20 22:39:41 +0600

    rout  done

--- src/app/modules/student/user.route.ts
+++ src/app/modules/student/user.route.ts
@@ -5,11 +5,11 @@ const router = express.Router();
 
 router.post('/', UserController.createUser);
 router.get('/:userId', UserController.getSingleUser);
-router.get('/:userId/orders/total-price', UserController.getTotalPriceOrders);
 router.get('/:userId/orders', UserController.getSingleUserOrders);
 router.put('/:userId/orders', UserController.createUserOrder);
 router.put('/:userId', UserController.updateUser);
 router.get('/', UserController.getAllUsers);
 router.delete('/:userId', UserController.deleteUser);
+router.get('/:userId/orders/total-price', UserController.getTotalPriceOrders);
 
 export const UserRoute = router;
